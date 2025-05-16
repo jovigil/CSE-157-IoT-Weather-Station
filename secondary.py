5fa948e0cea95e52aee50ed43a96f6ef7185bb86
joey vigil <jovigil@ucsc.edu>
2025-05-16 13:03:41 -0700

    fixed secondary

--- secondary.py
+++ secondary.py
@@ -26,8 +26,9 @@ WIND_MIN = 0.0
 WIND_MAX = 32.4
 
 
-HOST = "169.233.1.17" 
-PORT = 65425  # Port to listen on 
+HOST = "169.233.1.9"
+MY_IP = "169.233.1.17"
+PORT = 65406  # Port to listen on 
 
 
 sensor_data = {
@@ -39,52 +40,40 @@ sensor_data = {
 }
 
 
-# def get_port_assigned():
-#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def start():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#     s.connect((HOST, PORT))
+    s.connect((HOST, PORT))
 
-#     s.sendall(b"Hello, world")
-#     UPORT = int.from_bytes(s.recv(1024), byteorder='big')
-#     print(f"Received port number {UPORT}")
-#     s.close()
-#     return UPORT
+    s.sendall(b"Hello, world")
+    s.close()
 
 def read_wind_speed(voltage):
     voltage = max(min(voltage, V_MAX), V_MIN)
     return map_range(voltage, V_MIN, V_MAX, WIND_MIN, WIND_MAX)
 
-async def read_sht30():
-    while True:
-        temperature = sht30.temperature
-        humidity = sht30.relative_humidity
-        logger_sht30.info(f"{datetime.datetime.now()}\nTemperature: {temperature:.2f} C\nHumidity: {humidity:.2f}%\n")
-        sensor_data["temperature"] = temperature
-        sensor_data["humidity"] = humidity
-        await asyncio.sleep(1)
+def read_sht30():
+    temperature = sht30.temperature
+    humidity = sht30.relative_humidity
+    sensor_data["temperature"] = temperature
+    sensor_data["humidity"] = humidity
 
-async def read_stemma():
-    while True:
-        soil_moisture = soil_sensor.moisture_read()
-        soil_temp = soil_sensor.get_temp()
-        logger_stemma.info(f"{datetime.datetime.now()}\nSoil Temperature: {soil_temp:.2f} C\nSoil moisture: {soil_moisture:.2f}\n")
-        sensor_data["soil_moisture"] = soil_moisture
-        sensor_data["soil_temp"] = soil_temp
-        await asyncio.sleep(1)
-
-async def read_adc():
-    while True:
-        voltage = windspeed_channel.voltage
-        wind_speed = read_wind_speed(voltage)
-        logger_adc.info(f"{datetime.datetime.now()}\nWind speed: {wind_speed:.2f} m/s\n\n")
-        sensor_data["wind_speed"] = wind_speed
-        await asyncio.sleep(1)
+def read_stemma():
+    soil_moisture = soil_sensor.moisture_read()
+    soil_temp = soil_sensor.get_temp()
+    sensor_data["soil_moisture"] = soil_moisture
+    sensor_data["soil_temp"] = soil_temp
+        
+def read_adc():
+    voltage = windspeed_channel.voltage
+    wind_speed = read_wind_speed(voltage)
+    sensor_data["wind_speed"] = wind_speed
 
 async def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # UPORT = get_port_assigned()
+    start()
     global PORT
-    s.bind((HOST, PORT))
+    s.bind((MY_IP, PORT))
     s.listen()
     s.settimeout(5)
     i = 0
@@ -95,19 +84,27 @@ async def main():
             with conn:
                 data = conn.recv(1024)
                 print(f"Received {data!r} from {addr}")
-                # if i == 5:  #This is just for testing
-                #     time.sleep(5)
-                await asyncio.gather(read_sht30(),
-                             read_stemma(),
-                             read_adc()
-                            )
+                read_sht30()
+                read_stemma()
+                read_adc()
                 conn.send(json.dumps(sensor_data).encode())
         except (socket.timeout, ConnectionResetError, BrokenPipeError, OSError) as e:
             print(f"Error: {e}, reconnecting...")
-            s.close()
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.bind((HOST, PORT))
             s.listen()
             s.settimeout(3)
+
+if __name__ == "__main__":
+    # We will use a try/except block to catch the KeyboardInterrupt.
+    try:
+        """
+        Once we have defined our main coroutine, we will run it using asyncio.run().
+        """
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        """
+        If the user presses Ctrl+C, we will gracefully exit the program.
+        """
+        print("\nExiting program...")
+        exit(0)
 
 
